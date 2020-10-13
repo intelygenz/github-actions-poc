75731ed866f1eac21d23b366e823324fccc1696c
Sergio Fernandez <sergio.fernandez@intelygenz.com>
2020-10-13 12:41:08 +0200

    add greeting call

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,30 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import useAxios from 'axios-hooks'
 
 function App() {
+
   const [{ data, loading, error }, executeGet] = useAxios(
     {
-      url: 'http://localhost:4000',
-      method: 'GET'
+      url: `http://localhost:4000/greetings`,
+      method: 'POST',
+
     },
     {
       manual: true
     }
   ) 
 
+  const [name, setName] = useState("")
+
   const fetchData = async () => {
-    const response = await executeGet()
+    const response = await executeGet({data: {name}})
     console.log(response)
   }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>POC WORKFLOW DEMO FRONT</h1>
+        <h3>Insert your name</h3>
+        <input type="text" onChange={(e) => setName(e.target.value)}/>
         {/* Show Loading or Call */}
         { (() => {
           if(loading) return <h3>Loading...</h3>
-          else return <a onClick={fetchData}>Call to Back</a>
+          else return <button onClick={fetchData}>Call to Back</button>
         })()}
         {/* Show data or error */}
         { (() => {
