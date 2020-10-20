9a78becb0a45831ce8e76e711d6316b671c6b51a
Sergio Fernandez <sergio.fernandez@intelygenz.com>
2020-10-20 09:05:49 +0200

    test prerelease

--- action-pre-release/index.js
+++ action-pre-release/index.js
@@ -1,26 +1,127 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+const octokit = github.getOctokit(process.env.GITHUB_TOKEN)
+const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
 
-  getData()
+const dryRun = Boolean(core.getInput('dry-run'));
+const prefix = core.getInput('prefix')
+const preRelease = core.getInput('pre-release');
 
-console.log("hola")
-} catch (error) {
-  core.setFailed(error.message);
+main()
+
+async function main() {
+  try {    
+    let release = await calcReleaseBranch(prefix)
+  
+    if (preRelease) {
+      console.log("Generating prerelease")
+      release = await calcTagBranch(release)
+    }
+
+    core.setOutput("release", release);
+  
+    if (!dryRun) createTag(release)
+  
+    console.log("New Release: ",release)
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
-async function getData () {
-  const octokit = github.getOctokit("722f0e425af1269f20a8684726d22e6d35fe0285")
-  // const { data: pullRequest } = await octokit.pulls.get({})
-  console.log(`***----GITHUB CONTEXT: ${JSON.stringify(octokit, null, 2)}`)
 
+async function calcReleaseBranch(prefix) {
+  try {
+    const { data:branches } = await octokit.repos.listBranches({
+      owner,
+      repo
+    });
+
+    let mayor = 0, minor = 0;
+
+    const branchesWithPrefix = branches.filter(branch => branch.name.match(`^${prefix}`)).reverse()
+    if(branchesWithPrefix.length === 0) {
+      return `${prefix}${mayor}.${minor}`
+    }
+
+    const regex = new RegExp(`^${prefix}(\\d).(\\d)$`, 'g')
+    const releaseBranch = branchesWithPrefix[0]
+    const matches = regex.exec(releaseBranch.name)
+    mayor = parseInt(matches[1]);
+    minor = parseInt(matches[2]);
+    return `${prefix}${mayor}.${minor+1}`
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+async function calcTagBranch(release) {
+  try {
+    const { data:tags } = await octokit.repos.listTags({
+      owner,
+      repo
+    });
+
+    const tagsWithPrefix = tags.filter(tag => tag.name.match(`^${release}-${preRelease}`)).reverse()
+    if(tagsWithPrefix.length === 0) return `${release}-${preRelease}.0`
+
+    const regex = new RegExp(`^${release}-${preRelease}.(\\d)$`, 'g')
+    const releaseTag = tagsWithPrefix[0]
+    const matches = regex.exec(releaseTag.name)
+    bumpVersion = parseInt(matches[1]);
+    return `${release}-${prerelease}.${bumpVersion+1}`
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
+async function createTag(tagName) {
+  try {
+    const { data:branchData } = await octokit.repos.listBranches({
+      owner,
+      repo
+    });
+    const mainBranchSHA = branchData.filter((elem) => {
+      if (elem.name === 'main') return elem
+    })[0].commit.sha
+    console.log("SHA", mainBranchSHA)
+    const { data:tagData } = await octokit.git.createTag({
+      owner,
+      repo,
+      tag: tagName,
+      message: "tag message",
+      object: mainBranchSHA,
+      type: "commit"
+    });
+    console.log("DATA TAG: ",JSON.stringify(tagData, null, 2))
+    const { data:crateTagData } = await octokit.git.createRef({
+      owner,
+      repo,
+      ref: `refs/tags/${tagName}`,
+      sha: tagData.sha,
+      object: mainBranchSHA,
+      type: "commit"
+    });
+    console.log("CREATE DATA TAG: ",JSON.stringify(crateTagData, null, 2))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+async function searchTag(tagName) {
+  try {
+    const { data: tag} = await octokit.git.getRef({
+      owner,
+      repo,
+      ref: `tags/${tagName}`
+    })
+    console.log(tag)
+
+  } catch (error) {
+    console.log(error)
+  }
 }
